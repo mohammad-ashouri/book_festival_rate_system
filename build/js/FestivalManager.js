0d888b2e2f6f742b865d6a72b66858ccd802fb0c
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-06-09 19:22:44 +0430

    Fix Festival Manager Checking Fields

--- build/js/FestivalManager.js
+++ build/js/FestivalManager.js
@@ -1,6 +1,6 @@
 if (document.getElementById('finishFestival')) {
     document.getElementById('finishFestival').onclick = function () {
-        if (confirm('این عملیات قابل بازگشت نیست، آیا مطمئن هستید؟')){
+        if (confirm('این عملیات قابل بازگشت نیست، آیا مطمئن هستید؟')) {
             $.ajax({
                 url: "build/php/inc/FestivalManager.php",
                 type: "POST",
@@ -8,7 +8,7 @@ if (document.getElementById('finishFestival')) {
                     work: 'Finish_Festival',
                 },
                 success: function (response) {
-                    if (response==='Done'){
+                    if (response === 'Done') {
                         location.reload();
                     }
                 }
@@ -19,48 +19,58 @@ if (document.getElementById('finishFestival')) {
 
 if (document.getElementById('New_Festival')) {
     document.getElementById('New_Festival').onclick = function () {
-        // if (confirm('این عملیات قابل بازگشت نیست، آیا مطمئن هستید؟')){
-        var festival_name=document.getElementById('festival_name').value;
-        var start_date=document.getElementById('start_date').value;
-        var password=document.getElementById('password').value;
-        function convertDigitsToFarsi(input) {
-            const persianDigits = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
-            const englishDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        var festival_name = document.getElementById('festival_name').value;
+        var start_date = document.getElementById('start_date').value;
+        var password = document.getElementById('password').value;
+        if (!festival_name) {
+            alert('نام فارسی دوره وارد نشده است.');
+            return false;
+        } else if (!start_date) {
+            alert('تاریخ شروع فراخوان انتخاب نشده است.');
+            return false;
+        } else {
+            if (confirm('این عملیات قابل بازگشت نیست، آیا مطمئن هستید؟')) {
 
-            for (let i = 0; i < persianDigits.length; i++) {
-                input = input.replace(persianDigits[i], englishDigits[i]);
-            }
+                function convertDigitsToFarsi(input) {
+                    const persianDigits = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
+                    const englishDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-            return input;
-        }
-        start_date = convertDigitsToFarsi(start_date);
-
-            $.ajax({
-                url: "build/php/inc/FestivalManager.php",
-                type: "POST",
-                data: {
-                    work: 'New_Festival',
-                    festivalName:festival_name,
-                    startDate:start_date,
-                    password: password,
-                },
-                success: function (response) {
-                    switch (response){
-                        case 'Done':
-                            alert('فراخوان با موفقیت ایجاد شد.');
-                            location.reload();
-                            break;
-                        case 'FestivalNameExists':
-                            alert('نام جشنواره تکراری است.');
-                            return false;
-                            break;
-                        case 'WrongPassword':
-                            alert('رمز عبور اشتباه است.');
-                            return false;
-                            break;
+                    for (let i = 0; i < persianDigits.length; i++) {
+                        input = input.replace(persianDigits[i], englishDigits[i]);
                     }
+
+                    return input;
                 }
-            });
-        // }
+
+                start_date = convertDigitsToFarsi(start_date);
+
+                $.ajax({
+                    url: "build/php/inc/FestivalManager.php",
+                    type: "POST",
+                    data: {
+                        work: 'New_Festival',
+                        festivalName: festival_name,
+                        startDate: start_date,
+                        password: password,
+                    },
+                    success: function (response) {
+                        switch (response) {
+                            case 'Done':
+                                alert('فراخوان با موفقیت ایجاد شد.');
+                                location.reload();
+                                break;
+                            case 'FestivalNameExists':
+                                alert('نام جشنواره تکراری است.');
+                                return false;
+                                break;
+                            case 'WrongPassword':
+                                alert('رمز عبور اشتباه است.');
+                                return false;
+                                break;
+                        }
+                    }
+                });
+            }
+        }
     }
 }
