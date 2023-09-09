96fcacb6e461d704d23597919798eda59da2d7af
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-09-09 09:21:30 +0330

    Add SummaryAssessmentManager Index Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AssessmentController;
 use App\Http\Controllers\Catalogs\FestivalController;
 use App\Http\Controllers\Catalogs\LanguageController;
 use App\Http\Controllers\Catalogs\ScientificGroupController;
@@ -89,23 +90,13 @@ Route::middleware(CheckLoginMiddleware::class)->middleware(MenuMiddleware::class
             Route::post('/changeScientificGroup', [PostController::class, 'changeScientificGroup']);
             Route::post('/Classification', [PostController::class, 'Classification']);
 
+            Route::get('/SummaryAssessmentManager', [AssessmentController::class, 'summaryAssessmentIndex']);
+
+
+
         });
         Route::middleware('roleAuthorization:2')->group(function () {
-//            Route::get('/Person', [PersonController::class, 'index']);
-//            Route::post('/newPerson', [PersonController::class, 'newPerson'])->name('newPerson');
-//            Route::get('/getPersonInfo', [PersonController::class, 'getPersonInfo']);
-//            Route::post('/editPerson', [PersonController::class, 'editPerson']);
-//
-//            Route::get('/Posts', [PostController::class, 'index']);
-//            Route::post('/newPost', [PostController::class, 'newPost']);
-//            Route::get('/getPostInfo', [PostController::class, 'getPostInfo']);
-//            Route::get('/getParticipants', [PostController::class, 'getParticipants']);
-//            Route::post('/editPost', [PostController::class, 'editPost']);
-//            Route::delete('/deletePost', [PostController::class, 'deletePost']);
-//
-//            Route::get('/Classification', [PostController::class, 'showClassification']);
-//            Route::post('/changeScientificGroup', [PostController::class, 'changeScientificGroup']);
-//            Route::post('/Classification', [PostController::class, 'Classification']);
+
         });
 
     });
