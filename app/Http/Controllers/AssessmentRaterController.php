f0760d0c364f1b3a91f97547ae51f081759d88e6
Mohammad <exploiter0253@gmail.com>
2023-12-21 11:05:51 +0330

    Fix showing posts where sg1_form_type and sg2_form_type not equal to waiting for header

--- app/Http/Controllers/AssessmentRaterController.php
+++ app/Http/Controllers/AssessmentRaterController.php
@@ -218,7 +218,18 @@ class AssessmentRaterController extends Controller
 
     public function summaryAssessmentIndex()
     {
-        $summaries = RateInfo::with('postInfo')->where('rate_status', 'Summary')->where('sg1_form_type', '!=', 'Waiting For Header')->paginate(10);
+        $summaries = RateInfo::with('postInfo')
+            ->where('rate_status', 'Summary')
+            ->where(function ($query) {
+                $query->whereNull('sg2_form_type')
+                    ->where('sg1_form_type', '!=', 'Waiting For Header');
+            })
+            ->orWhere(function ($query) {
+                $query->whereNotNull('sg2_form_type')
+                    ->where('sg1_form_type', '!=', 'Waiting For Header')
+                    ->where('sg2_form_type', '!=', 'Waiting For Header');
+            })
+            ->paginate(20);
         return view('SummaryAssessmentAdmin', compact('summaries'));
     }
 
