d83869792deaa5c49a96c6de18b643c646a6bcbc
Me Home <exploiter0253@gmail.com>
2024-03-11 18:25:49 +0330

    Add publishers menu + Fix catalogs menu

--- resources/views/layouts/PanelMaster.blade.php
+++ resources/views/layouts/PanelMaster.blade.php
@@ -174,7 +174,7 @@
                                     <svg
                                         class="w-5 h-5 dark:text-white transition duration-75 dark:group-hover:text-gray-700"
                                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                        viewBox="0 0 25 25">
+                                        viewBox="0 0 25 25" height="25px" width="25px">
                                         <path
                                             d="M23.121,9.069,15.536,1.483a5.008,5.008,0,0,0-7.072,0L.879,9.069A2.978,2.978,0,0,0,0,11.19v9.817a3,3,0,0,0,3,3H21a3,3,0,0,0,3-3V11.19A2.978,2.978,0,0,0,23.121,9.069ZM15,22.007H9V18.073a3,3,0,0,1,6,0Zm7-1a1,1,0,0,1-1,1H17V18.073a5,5,0,0,0-10,0v3.934H3a1,1,0,0,1-1-1V11.19a1.008,1.008,0,0,1,.293-.707L9.878,2.9a3.008,3.008,0,0,1,4.244,0l7.585,7.586A1.008,1.008,0,0,1,22,11.19Z"/>
                                     </svg>
@@ -194,7 +194,7 @@
                                                 <svg
                                                     class="ml-3 flex-shrink-0 w-5 h-5  transition duration-75 text-cu-light group-hover:text-cu-light dark:group-hover:text-gray-700"
                                                     aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                    fill="currentColor" viewBox="0 0 25 25">
+                                                    fill="currentColor" viewBox="0 0 25 25" height="25px" width="25px">
                                                     @if(@$menu['path1'])
                                                         <path
                                                             d="{{ $menu['path1'] }}"></path>
@@ -232,7 +232,7 @@
                                                                 class="w-5 h-5 dark:text-white transition duration-75 dark:group-hover:text-gray-700"
                                                                 aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                                                 fill="currentColor"
-                                                                viewBox="0 0 25 25">
+                                                                viewBox="0 0 25 25" height="25px" width="25px">
                                                                 <path
                                                                     d="{{ @$child['path1'] }}"></path>
                                                                 <path
