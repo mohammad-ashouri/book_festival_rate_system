7d34b55a5091587420852c73ad240f8e020070a5
Me Home <exploiter0253@gmail.com>
2024-03-15 17:36:56 +0330

    Fix edit person button name

--- resources/views/PersonManager.blade.php
+++ resources/views/PersonManager.blade.php
@@ -232,7 +232,7 @@
                                 <td class="px-6 py-4">
                                     <button type="submit" data-id="{{ $person->id }}"
                                             class="px-4 py-2 mr-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 PersonControl">
-                                        جزئیات و ویرایش
+                                        ویرایش
                                     </button>
                                 </td>
                             </tr>
