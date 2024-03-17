164ceade5b2e3a9af8f8b5858d7915c7b0b3a394
Me Home <exploiter0253@gmail.com>
2024-03-17 22:22:56 +0330

    Add fillable fields to festival model

--- app/Models/Catalogs/Festival.php
+++ app/Models/Catalogs/Festival.php
@@ -10,5 +10,13 @@ class Festival extends Model
 {
     use HasFactory,SoftDeletes;
     protected $table='festivals';
+    protected $fillable = [
+        'name',
+        'start_date',
+        'starter',
+        'finish_date',
+        'finisher',
+        'status',
+    ];
     protected $hidden=['created_at','updated_at','deleted_at'];
 }
