5f6b7e98f1ac9f3843f1d84081f9354f51b10166
Mohammad <exploiter0253@gmail.com>
2024-03-17 12:24:56 +0330

    Fix database seeders from sql

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             FestivalSeeder::class,
+            ImportSQLSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/ImportSQLSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImportSQLSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::unprepared(file_get_contents('database/migrations/users.sql'));
+        DB::unprepared(file_get_contents('database/migrations/general_informations.sql'));
+    }
+}
