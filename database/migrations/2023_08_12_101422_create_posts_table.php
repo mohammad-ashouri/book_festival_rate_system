5037325d48ba02e471b32958bea023470b95eb90
Mohammad <exploiter0253@gmail.com>
2024-03-17 12:13:24 +0330

    Change persons to users into posts migration

--- database/migrations/2023_08_12_101422_create_posts_table.php
+++ database/migrations/2023_08_12_101422_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('persons');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedBigInteger('festival_id');
             $table->foreign('festival_id')->references('id')->on('festivals');
             $table->text('title');
